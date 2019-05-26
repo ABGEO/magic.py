5d3cb878470c21ea975aafe0510af1ba569ac19e
ABGEO <takalandzet@gmail.com>
2019-05-26 15:14:58 +0400

    Add base functionality.

--- magic.py
+++ magic.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #                        _
 #  _ __ ___   __ _  __ _(_) ___   _ __  _   _
 # | '_ ` _ \ / _` |/ _` | |/ __| | '_ \| | | |
@@ -7,9 +9,46 @@
 # v1.0.0
 #                        <Directed ByABGEO />
 
+import random
+
+
+def get_random_answer():
+    """Get random answer.
+
+    Returns:
+        Random answer from answers array.
+    """
+
+    # Generate random number
+    rand = random.randint(0, 7)
+
+    answers = [
+        "უეჭველი.",
+        "ამ ცხოვრებაში არა.",
+        "შენ ამას შეძლებ.",
+        "სხვა დროს მკითხე.",
+        "კონცენტრირდი და ხელახლა მკითხე.",
+        "რატომ მაბრაზებ?! სხვა რამე მკითხე.",
+        "ჩემი პასუხია - არა.",
+        "რატომაც არა.",
+    ]
+
+    # Return random answer from array
+    return answers[rand]
+# End get_random_answer()
+
 
 def main():
-    print('Init')
+    question = input('მკითხე რამე ან დააჭირე Enter-ს გამოსასვლელად: ')
+
+    while question != '':
+        print('\t- ' + get_random_answer())
+
+        question = input('\nკიდევ მკითხე რამე ან დააჭირე Enter-ს გამოსასვლელად: ')
+    # End while
+
+    print('\n\nმომავალ შეხვედრამდე! გელოდები ახალი კითხვებით.')
+# End main()
 
 
 if __name__ == "__main__":
